f265ed534d438bc36092e04f30c908a29fe8ad51
bolshakov.mp <email@bk.ru>
2025-09-02 01:03:40 +0300

    follow Flake8

--- Game/class_Character.py
+++ Game/class_Character.py
@@ -6,7 +6,6 @@ DEFAULT_DEFENCE = 10
 DEFAULT_STAMINA = 5
 
 
-
 class Character:
 
     BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
@@ -27,16 +26,15 @@ class Character:
         return (f'{self.name} блокировал {value_defence} урона')
 
     def special(self):
-        return (f'{self.name} применил специальное умение ' \
+        return (f'{self.name} применил специальное умение '
                 f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
-    
-    def __str__(self):
-        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.' 
 
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
 
 
 class Warrior(Character):
-    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. ' \
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
                              'Сильный, выносливый и отважный')
     RANGE_VALUE_ATTACK = (3, 5)
     RANGE_VALUE_DEFENCE = (5, 10)
@@ -45,7 +43,7 @@ class Warrior(Character):
 
 
 class Mage(Character):
-    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. ' \
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
                              'Обладает высоким интеллектом')
     RANGE_VALUE_ATTACK = (5, 10)
     RANGE_VALUE_DEFENCE = (-2, 2)
@@ -54,26 +52,30 @@ class Mage(Character):
 
 
 class Healer(Character):
-    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
-                             'Черпает силы из природы, веры и духов')
+    BRIEF_DESC_CHAR_CLASS = (
+        ' могущественный заклинатель. '
+        'Черпает силы из природы, веры и духов'
+        )
     RANGE_VALUE_ATTACK = (-3, -1)
     RANGE_VALUE_DEFENCE = (2, 5)
     SPECIAL_BUFF = DEFAULT_DEFENCE + 30
-    SPECIAL_SKILL = 'Защита' 
-
+    SPECIAL_SKILL = 'Защита'
 
 
 def choice_char_class(char_name: str) -> Character:
 
     game_classes = {'warrior': Warrior, 'mage': Mage, 'healer': Healer}
-    approve_choice: str  = None
+    approve_choice: str = None
 
     while approve_choice != 'y':
-        selected_class = input('Введи название персонажа, '
-                           'за которого хочешь играть: Воитель — warrior, '
-                           'Маг — mage, Лекарь — healer: ')
+        selected_class = input(
+                                'Введи название персонажа, '
+                                'за которого хочешь играть: Воитель — warrior,'
+                                ' Маг — mage, Лекарь — healer: '
+                            )
         if selected_class in game_classes:
-            character_class: Character = game_classes[selected_class](char_name)
+            character_class: Character = \
+                            game_classes[selected_class](char_name)
         else:
             continue
         print(character_class)
@@ -88,9 +90,11 @@ def start_training(character):
     print(character)
 
     print('Потренируйся управлять своими навыками.')
-    print('Введи одну из команд: attack — чтобы атаковать противника, '
-          'defence — чтобы блокировать атаку противника или '
-          'special — чтобы использовать свою суперсилу.')
+    print(
+        'Введи одну из команд: attack — чтобы атаковать противника, '
+        'defence — чтобы блокировать атаку противника или '
+        'special — чтобы использовать свою суперсилу.'
+        )
     print('Если не хочешь тренироваться, введи команду skip.')
 
     cmd = None
@@ -104,7 +108,6 @@ def start_training(character):
         if cmd in character_actions:
             print(character_actions[cmd]())
     return 'Тренировка окончена.'
-
 
 
 if __name__ == '__main__':
