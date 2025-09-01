349978556b3f7fe841df01980e4b6d72bf6ae5bd
bolshakov.mp <email@bk.ru>
2025-09-01 21:21:47 +0300

    release class Bird, Parrot. Penguin

---
+++ OOP/class_Bird.py
@@ -0,0 +1,16 @@
+class Bird:
+    
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+    
+    def describe(self):
+        print(f"Размер птицы {self.name} - {self.size}")
+
+
+class Parrot(Bird):
+    pass
+
+
+class Penguin(Bird):
+    pass
